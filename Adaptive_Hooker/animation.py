4de8b46896c60eb5193503c60dd686574b397b9c
Chaojie Feng <fcj941116@gmail.com>
2018-11-14 11:59:05 -0800

    modified animation/add hook

--- Adaptive_Hooker/animation.py
+++ Adaptive_Hooker/animation.py
@@ -1,13 +1,15 @@
 from utility import *
 
 
-def animation(x, theta_b):
+def animation(x, theta_b, target_point):
     """
     :param x: block state variable input as list of tuple [t1, t2, t3 ...] with
      each tuple ti specified as (x, y, theta). x is position along x axis, y is
      position along y axis, theta is angle for block
 
     :param theta_b: list of angle state for beam [a1, a2, a3 ...]
+
+    :param target_point: target point for hook (x,y)
 
     :return: None
     """
@@ -20,16 +22,22 @@ def animation(x, theta_b):
 
     # define balance beam
     length_b = 1
-    width_b = 13
+    width_b = 15
     dl_b = 0.5 * np.sqrt(length_b**2 + width_b**2)
     offset_b = - np.pi/2 - np.arctan(length_b/width_b)
 
 
     # define lower left corner point of block
-    angle_i = np.deg2rad(x[0][2])
+    angle_patch = x[0][2]
+    angle_i = np.deg2rad(angle_patch)
     pos_i = (x[0][0], x[0][1])
     ri = (dl * np.cos(offset + angle_i), dl * np.sin(offset + angle_i))
     pi = (pos_i[0] + ri[0], pos_i[1] + ri[1])
+
+    # define points of power board
+
+    p_powerboard = ( -(6-6*np.tan(angle_i)) * np.cos(angle_i) , -(6/np.cos(angle_i)+(6-6*np.tan(angle_i)) * np.sin(angle_i)))
+    p_powercircle = ( -(7-5*np.tan(angle_i)) * np.cos(angle_i) , -(5/np.cos(angle_i)+(7-5*np.tan(angle_i)) * np.sin(angle_i)))
 
     # define hook
 
@@ -37,11 +45,12 @@ def animation(x, theta_b):
     pos_line2 = (x[0][1] + 0.5* width * np.cos(angle_i), x[0][1] + (0.5* width + 1) * np.cos(angle_i))
 
     pos_arc0 = (x[0][0] - (0.5* width+1) * np.sin(angle_i), x[0][1] + (0.5* width + 1) * np.cos(angle_i))
-    pos_arc1 = (x[0][0] - (0.5* width + 2 - np.tan(angle_i)) * np.sin(angle_i) - 2/np.cos(angle_i), x[0][1] + (0.5* width + 2 - np.tan(angle_i)) * np.cos(angle_i))
-    pos_arc2 = (x[0][0]- (0.5* width + 3) * np.sin(angle_i), x[0][1] + (0.5* width + 3) * np.cos(angle_i))
+    pos_arc1 = (x[0][0] - (0.5* width + 2 - np.tan(angle_i)) * np.sin(angle_i) - 3/np.cos(angle_i), x[0][1] + (0.5* width + 2 - np.tan(angle_i)) * np.cos(angle_i))
+    pos_arc2 = (x[0][0] - (0.5* width + 3) * np.sin(angle_i), x[0][1] + (0.5* width + 3) * np.cos(angle_i))
 
     # define lower left corner point of beam
     theta_bi = np.deg2rad(theta_b[0])
+    angle_beam = angle_patch + theta_b[0]
     angle_bi = angle_i + theta_bi
     rbi = (dl_b * np.cos(offset_b + angle_bi), dl_b * np.sin(offset_b + angle_bi))
     pbi = (pos_i[0] + rbi[0], pos_i[1] + rbi[1])
@@ -49,28 +58,29 @@ def animation(x, theta_b):
     # initialize plot
     fig = plt.figure()
     fig.set_size_inches(7, 6.5)
-    ax = plt.axes(xlim=(-20, 50), ylim=(-20, 50))
-    patch = plt.Rectangle(pi, length, width, angle_i, fc='r')
-    beam = plt.Rectangle(pbi, length_b, width_b, angle_bi, fc='y')
+    ax = plt.axes(xlim=(-20, 100), ylim=(-20, 100))
+    patch = mpatches.Rectangle(pi, length, width,angle_patch , fc='r')
+    beam = mpatches.Rectangle(pbi, length_b, width_b, angle_beam, fc='y')
+    Power_board = mpatches.Rectangle(p_powerboard, 12, 2, angle_patch, fc='b')
+    Power_circle = mpatches.Circle(p_powercircle,radius=2,color='b')
+    target_circle = mpatches.Circle(target_point, radius=0.7, color='green')
     line = matplotlib.lines.Line2D(pos_line1,pos_line2,linewidth=2, color='k')
     Path = mpath.Path([pos_arc0,pos_arc1,pos_arc2], [1, 3, 3])
     arc = mpatches.PathPatch(Path)
+    arc.set_fill(False)
 
 
     def init():
-        #patch.xy = pi
-        #patch.angle = np.rad2deg(angle_i)
+
         ax.add_patch(patch)
-
-        #beam.xy = pbi
-        #beam.angle = np.rad2deg(angle_bi)
         ax.add_patch(beam)
-
-
+        ax.add_patch(Power_board)
+        ax.add_patch(Power_circle)
+        ax.add_patch(target_circle)
         ax.add_line(line)
-
-
         ax.add_patch(arc)
+
+
 
         return patch, beam, line, arc
 
@@ -104,19 +114,20 @@ def animation(x, theta_b):
 
         posi_line2 = (x[i][1] + 0.5 * width * np.cos(angle), x[i][1] + (0.5 * width + 1) * np.cos(angle))
 
-        posi_arc0 = (x[i][0] - (0.5 * width + 1) * np.sin(angle),x[i][1] + (0.5 * width + 1) * np.cos(angle))
+        posi_arc0 = ( x[i][0] - (0.5 * width + 1) * np.sin(angle),x[i][1] + (0.5 * width + 1) * np.cos(angle))
 
-        posi_arc1 = (x[i][0] - (0.5 * width + 2 - np.tan(angle)) * np.sin(angle) - 2 / np.cos(angle),
+        posi_arc1 = (x[i][0] - (0.5 * width + 2 - np.tan(angle)) * np.sin(angle) - 3 / np.cos(angle),
                     x[i][1] + (0.5 * width + 2 - np.tan(angle)) * np.cos(angle))
+
         posi_arc2 = (x[i][0] - (0.5 * width + 3) * np.sin(angle), x[i][1] + (0.5 * width + 3) * np.cos(angle))
 
         line.set_data(posi_line1,posi_line2)
 
         arc._path = mpath.Path([posi_arc0, posi_arc1, posi_arc2], [1, 3, 3])
-
+        arc.set_fill(False)
         return patch, beam, line, arc
 
-    _ = FuncAnimation(fig, animate, init_func=init, frames=len(x), interval=1000, blit=False)
+    _ = FuncAnimation(fig, animate, init_func=init, frames=len(x), interval=20, blit=False)
     plt.show()
 
 
--- Adaptive_Hooker/dynamics.py
+++ Adaptive_Hooker/dynamics.py
@@ -2,48 +2,53 @@ from animation import animation
 from utility import *
 
 
-x = [(0, 0, 0), (11, 11, 18), (12, 12, 16), (13, 13, 14), (14, 14, 12)]
-t = [10, 20, 25, 20, 15]
+#x = [(0, 0, 20), (11, 11, 18), (12, 12, 16), (13, 13, 14), (14, 14, 0)]
+#t = [20, 30, 40, 50, 60]
 
 
 class MyController:
-    def __init__(self, u=(3, 3), block_mass = 1, beam_mass = 1, initial_state = (0, 0, 0), target_state = (20, 20, 0)):
+    def __init__(self, u=(40, 40), block_mass = 1, beam_mass = 1, initial_state = (0, 0, 0), target_state = (40, 40, 0)):
         """
 
         :param u: 2 member list style input variable contains [velocity, direction]
         """
         self.u = u
+        self.angle = np.deg2rad(self.u[1])
         self.mBlock = block_mass
         self.mBeam = beam_mass
         self.xi = initial_state
-        self.xf = target_state-(4,4,0)
+        self.target_state = target_state
         self.a = np.array((0.0, -9.81))
-        self.dt = 0.1
-        self.traj = []
+        self.dt = 0.01
         self.T = 0
 
     def dynamics(self):
-        V_vert = self.u[0]*math.sin(self.u[1])
-        V_hori = self.u[0]*math.cos(self.u[1])
-        self.T = int(self.xf[0]-self.xi[0]/V_hori)
-        height = V_vert*self.T+0.5 * self.a * self.T**2
-        if height != self.xf[2]:
+        traj = []
+        xf = (self.target_state[0], self.target_state[1]-5,0)
+        angle_r = np.deg2rad(self.u[1])
+        V_vert = self.u[0] * np.sin(angle_r)
+        V_hori = self.u[0] * np.cos(angle_r)
+        self.T = (xf[0]-self.xi[0])/V_hori
+        height = V_vert*self.T+0.5 * self.a[1] * self.T**2
+        if height != xf[2]:
             print('cant reach target')
         time_line = np.arange(0, self.T, self.dt)
-        pos = (0, 0)
+        pos = [0, 0]
         V_vert_temp = V_vert
         for t in time_line:
-            pos[0] = pos[0]+V_hori*dt
-            pos[1] = pos[1]+0.5 * self.a * dt**2 + V_vert_temp * dt
-            V_vert_temp = V_vert_temp - self.a * dt
-            self.traj.append(pos[0], pos[1], 0)
-        return self.traj
+            pos[0] = pos[0]+V_hori*self.dt
+            pos[1] = pos[1]+0.5 * self.a[1] * self.dt**2 + V_vert_temp * self.dt
+            V_vert_temp = V_vert_temp + self.a[1] * self.dt
+            traj.append((pos[0], pos[1], self.u[1]))
+        return traj
 
 
-animation(x, t)
-#if __name__ == '__main__':
-
-
+#animation(x, t,(14,20))
+if __name__ == '__main__':
+    object =  MyController()
+    x = object.dynamics()
+    t= np.ones(len(x))
+    animation(x,t, object.target_state)
 
 
 
