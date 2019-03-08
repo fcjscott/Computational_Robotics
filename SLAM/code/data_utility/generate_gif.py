180e2a9a0c1f5c5b2314ae77e20785c046f4d47f
Chaojie Feng <fcj941116@gmail.com>
2019-03-08 11:31:24 -0800

    removed stuff

--- SLAM/code/data_utility/generate_gif.py
+++
@@ -1,9 +0,0 @@
-from data_utility.utility import *
-def generate_gif(img_sav_dir):
-    png_dir = img_sav_dir
-    images = []
-    for file_name in sorted(os.listdir(img_sav_dir), key = lambda x: int(re.findall(r'\d+', x)[0])):
-        #if file_name.endswith('.png'):
-        file_path = os.path.join(png_dir, file_name)
-        images.append(imageio.imread(file_path))
-    imageio.mimsave('em_slam_{}.gif'.format(img_sav_dir), images, duration = 0.5)
